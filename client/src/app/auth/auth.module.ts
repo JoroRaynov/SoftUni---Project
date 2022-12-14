af1b69752ec60ad1e2ebfb9407818d0f6b81cc2b
Georgi Raynov <jornalo891@gmail.com>
2022-12-14 19:35:21 +0200

    add logout

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LogoutComponent } from './logout/logout.component';
 
 
 
 @NgModule({
   declarations: [
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     CommonModule,
