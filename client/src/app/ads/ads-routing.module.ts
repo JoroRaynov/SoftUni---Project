36a936dca94a6487fb445a59ad043e8ba30142a4
Georgi Raynov <jornalo891@gmail.com>
2022-12-15 14:45:40 +0200

    add create form

--- client/src/app/ads/ads-routing.module.ts
+++ client/src/app/ads/ads-routing.module.ts
@@ -1,10 +1,15 @@
 import { RouterModule, Routes } from "@angular/router";
 import { AdsListComponent } from "./ads-list/ads-list.component";
+import { NewAdComponent } from "./new-ad/new-ad.component";
 
 const routes: Routes = [
     {
         path: 'data/catalog',
         component: AdsListComponent
+    },
+    {
+        path: 'data/catalog/new',
+        component: NewAdComponent
     }
 ]
 
--- client/src/app/ads/ads.module.ts
+++ client/src/app/ads/ads.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdsListComponent } from './ads-list/ads-list.component';
 import { AdsRoutingModule } from './ads-routing.module';
+import { NewAdComponent } from './new-ad/new-ad.component';
 
 
 
 @NgModule({
   declarations: [
-    AdsListComponent
+    AdsListComponent,
+    NewAdComponent
 
   ],
   imports: [
