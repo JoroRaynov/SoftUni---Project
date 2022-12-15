f14bb7eb0801d3f52d919ee4c2a482cac1a15b2a
Georgi Raynov <jornalo891@gmail.com>
2022-12-15 11:40:24 +0200

    add 404 page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [{
   path: '',
   pathMatch: 'full',
   component: MainComponent
-}];
+},
+{
+  path: 'not-found',
+  component: NotFoundComponent
+},
+{
+  path: '**',
+  redirectTo: '/not-found'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
