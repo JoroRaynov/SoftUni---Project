c6cd5f333d9d6c1db88df8cc7bd8a3d85b2b7341
Georgi Raynov <jornalo891@gmail.com>
2022-12-16 19:37:14 +0200

    add validation for register email

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  registerFormGroup: FormGroup = this.formBuilder.group({
+    email: new FormControl('', [Validators.required,Validators.minLength(8), Validators.pattern(/^.+@.+\..+$/)]),
+    tel: new FormControl(''),
+    passwords: new FormGroup({
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      rePassword: new FormControl(),
+    }),
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleRegister(): void {
+    console.log(this.registerFormGroup)
   }
 
 }
