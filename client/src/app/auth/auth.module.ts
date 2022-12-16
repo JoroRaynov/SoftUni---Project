c94a5e1f443e2610034cb2cb8eb298d7ad14d4f3
Georgi Raynov <jornalo891@gmail.com>
2022-12-16 13:11:06 +0200

    add

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LogoutComponent } from './logout/logout.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -13,11 +14,13 @@ import { ProfileComponent } from './profile/profile.component';
     RegisterComponent,
     LoginComponent,
     LogoutComponent,
-    ProfileComponent
+    ProfileComponent,
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
+
   ]
 })
 export class AuthModule { }
--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,21 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent  {
+export class LoginComponent {
 
-  constructor(private router: Router, private authService: AuthService) { 
-    this.authService.user  = {
-      email: 'john@gmail.com',
-      userName: 'John',
-    } as any;
-    this.router.navigate(['/']);
+  loginFormGroup: FormGroup = this.formBuilder.group({
+    'email': new FormControl('', [Validators.required, ]),
+    'password': new FormControl('', [Validators.required, Validators.minLength(6)])
+  }) 
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    // this.authService.user  = {
+    //   email: 'john@gmail.com',
+    //   userName: 'John',
+    // } as any;
+    // this.router.navigate(['/']);
   }
-
-  
+  loginHandler(): void{ 
+    console.log('form is submitted', this.loginFormGroup);
+  }
+  handleLogin(): void {
+    console.log('form must be submitted');
+  }
 }
