e86cd134498e0bc6311623f9ab7f4ce264d1da34
Georgi Raynov <jornalo891@gmail.com>
2022-12-22 11:26:39 +0200

    add guard for delete ad

--- client/src/app/ads/delete-ad/delete-ad.component.ts
+++ client/src/app/ads/delete-ad/delete-ad.component.ts
@@ -6,21 +6,33 @@ import { getSession } from 'src/app/auth/util/api';
 @Component({
   selector: 'app-delete-ad',
   templateUrl: './delete-ad.component.html',
-  styleUrls: ['./delete-ad.component.css']
+  styleUrls: ['./delete-ad.component.css'],
 })
 export class DeleteAdComponent implements OnInit {
-
   adId: string = '';
-  
-  constructor(private activatedRoute: ActivatedRoute,private router: Router, private apiService: ApiService) { }
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private apiService: ApiService
+  ) {}
 
   ngOnInit(): void {
     this.adId = this.activatedRoute.snapshot.params['id'];
     const userId = getSession().token._id;
 
-    this.apiService.deleteAd(this.adId).subscribe(data => {
+    this.apiService.loadAd(this.adId).subscribe({
+      next: (ad) => {
+        if (userId === ad._ownerId._id) {
+          this.apiService.deleteAd(this.adId).subscribe((data) => {});
+          this.router.navigate(['/']);
+        } else {
+          this.router.navigate(['/']);
+        }
+      },
+      error: (error) => {
+        console.log(error);
+      },
     });
-    this.router.navigate(['/']);
   }
-
 }
